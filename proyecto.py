e173dd6bbe9fa2c493afd46e5c344607447f1c60
LexMau <alexmmm1327@gmail.com>
2025-12-07 11:49:49 -0500

    screen preguntas

--- proyecto.py
+++ proyecto.py
@@ -4,12 +4,3 @@ numero_secreto = random.randint(1, 20)
 intentos = 5
 print("Bienvenido al juego de adivinar el numero!")
 print("Tengo un numero entre 1 y 20.")          
-for intento in range(1, intentos + 1):
-    adivinanza = int(input(f"Intento {intento} de {intentos}. ¿Cual es tu adivinanza? "))
-    if adivinanza < numero_secreto:
-        print("Tu adivinanza es muy baja.")
-    elif adivinanza > numero_secreto:
-        print("Tu adivinanza es muy alta.")
-    else:
-        print(f"¡Felicidades! Has adivinado el numero {numero_secreto} en {intento} intentos.")
-        break
